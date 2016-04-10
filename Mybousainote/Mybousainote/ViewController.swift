f5e78e2424be76c306fb81a42459181ca29c0a55
cockdoo <taigasano4112@gmail.com>
2016-04-10 16:04:03 +0900

    first commit

---
+++ Mybousainote/Mybousainote/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Mybousainote
+//
+//  Created by menteadmin on 2016/04/10.
+//  Copyright © 2016年 TaigaSano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        aaa()
+    }
+    
+    func aaa() {
+        
+    }
+
+}
+
